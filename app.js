7c57f757ebbf42bf07229a3b0841d354fb9549c8
Hitamashi <dothanhquocviet@yahoo.com.vn>
2017-12-02 03:10:02 +0700

    Update static link

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname +'/public'));
 
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
