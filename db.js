9095898189fc237009cf3db2ef5f09e94199e559
Hitamashi <dothanhquocviet@yahoo.com.vn>
2017-12-02 14:16:22 +0700

    Update db config

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 var mongoose = require('mongoose');
+var connectionString = process.env.MYSITE_MONGO || 'mongodb://user_rw:rwpassword@ds243055.mlab.com:43055/mydb';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://user_rw:rwpassword@ds243055.mlab.com:43055/mydb', { useMongoClient: true });
+mongoose.connect(connectionString, { useMongoClient: true });
